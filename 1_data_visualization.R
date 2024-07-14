61111d5a8b07913d52f095ac17aad421aad32187
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2024-07-14 21:16:51 +0200

    added gitignore

--- 1_data_visualization.R
+++ 1_data_visualization.R
@@ -1,3 +1,11 @@
 library(tidyverse)
 library(palmerpenguins)
 library(ggthemes)
+
+View(penguins)
+?penguins
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+)
